faeebb39ce28ba5e485408303446c1ebea455257
안재민 <jae7415@gmail.com>
2020-02-13 18:12:36 +0900

    Feature/install styled components (#4)
    * fea)Install styled-compoents and babel plugin for styled-components
    
    * fea)Set up babelrc configuration for styled-components
    
    * fea)Install styled-component types
    
    * fea)Set up _document.tsx for ues styled-component with SSR
    
    * etc)Test styled-components

--- src/frameworks/next/pages/index.tsx
+++ src/frameworks/next/pages/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const App: React.FC = () => <div>Hello Next.tsx</div>;
+const App: React.FC = () => <Wrapper>Hello Next.tsx</Wrapper>;
+
+const Wrapper = styled.div`
+    font-weight: bold;
+    margin: 0 auto;
+    width: 100%;
+    font-size: 200px;
+`;
 
 export default App;
